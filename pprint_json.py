3cbe9970c5cb314ed04c49ba6577d88a977ef676
Vasiliy2909 <vasiliyzh2909@yndex.ru>
2016-10-07 00:21:49 +0300

    Форматирование json

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,22 @@
 import json
+import os
+import pprint
 
 
 def load_data(filepath):
-    pass
+    if os.path.exists(filepath):
+        with open(filepath, 'r') as alco_shops:
+            return json.load(alco_shops)
 
 
 def pretty_print_json(data):
-    pass
+    num_data = int(input('введитеномер магазина='))
+    if 0 < num_data < 813:
+        pprint.pprint(data[num_data-1])
+    else:
+        print('Номер магазина не найден')
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data('alco_shops.json')
+    pretty_print_json(data)
