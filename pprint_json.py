8f27e653ae6b88e1c10d796f508af70f738e085f
Vasiliy2909 <vasiliyzh2909@yndex.ru>
2016-10-18 00:30:24 +0300

    исправлены ошибки

--- pprint_json.py
+++ pprint_json.py
@@ -3,20 +3,24 @@ import os
 import pprint
 
 
+def input_way_to_filepath():
+    way = input('Укажите путь к файлу:')
+    if os.path.exists(way):
+        return way
+
+
 def load_data(filepath):
     if os.path.exists(filepath):
         with open(filepath, 'r') as alco_shops:
-            return json.load(alco_shops)
+            data = json.load(alco_shops)
+            return data
 
 
 def pretty_print_json(data):
-    num_data = int(input('введитеномер магазина='))
-    if 0 < num_data < 813:
-        pprint.pprint(data[num_data-1])
-    else:
-        print('Номер магазина не найден')
+    pprint.pprint(data)
 
 
 if __name__ == '__main__':
-    data = load_data('alco_shops.json')
+    filepath = input_way_to_filepath()
+    data = load_data(filepath)
     pretty_print_json(data)
